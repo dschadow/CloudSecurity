59e1df63e7faf575adda5ba90ca3b8cd2c67af9b
Dominik Schadow <dominikschadow@gmail.com>
2020-02-16 09:37:26 +0100

    Renamed Application class

--- config-client-vault/src/main/java/de/dominikschadow/configclient/ConfigClientVaultApplication.java
+++ config-client-vault/src/main/java/de/dominikschadow/configclient/ConfigClientVaultApplication.java
@@ -17,20 +17,24 @@
  */
 package de.dominikschadow.configclient;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /**
- * Tests the application itself and tries to startup the application context.
+ * Main class to start the embedded web server and the Spring Boot application.
  *
  * @author Dominik Schadow
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class ApplicationTest {
-    @Test
-    public void contextLoads() {
+@SpringBootApplication
+@EnableConfigurationProperties(ConfigClientVaultProperties.class)
+public class ConfigClientVaultApplication {
+    /**
+     * Starts the config client application with the embedded Tomcat.
+     *
+     * @param args Runtime arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientVaultApplication.class, args);
     }
 }
--- config-client-vault/src/test/java/de/dominikschadow/configclient/ConfigClientVaultApplicationTest.java
+++ config-client-vault/src/test/java/de/dominikschadow/configclient/ConfigClientVaultApplicationTest.java
@@ -17,24 +17,20 @@
  */
 package de.dominikschadow.configclient;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
- * Main class to start the embedded web server and the Spring Boot application.
+ * Tests the application itself and tries to startup the application context.
  *
  * @author Dominik Schadow
  */
-@SpringBootApplication
-@EnableConfigurationProperties(ConfigClientVaultProperties.class)
-public class Application {
-    /**
-     * Starts the config client application with the embedded Tomcat.
-     *
-     * @param args Runtime arguments
-     */
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigClientVaultApplicationTest {
+    @Test
+    public void contextLoads() {
     }
 }
