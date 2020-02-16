7888b1be7f8e830609d099724d84aa7bc8a29c05
Dominik Schadow <dominikschadow@gmail.com>
2020-02-16 09:33:55 +0100

    Renamed Application class, removed start class configuration

--- config-server-vault/src/main/java/de/dominikschadow/configserver/vault/ConfigServerVaultApplication.java
+++ config-server-vault/src/main/java/de/dominikschadow/configserver/vault/ConfigServerVaultApplication.java
@@ -28,13 +28,13 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
-public class Application {
+public class ConfigServerVaultApplication {
     /**
      * Starts the config server application with the embedded Tomcat.
      *
      * @param args Runtime arguments
      */
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ConfigServerVaultApplication.class, args);
     }
 }
--- config-server-vault/src/test/java/de/dominikschadow/configserver/vault/ConfigServerVaultApplicationTest.java
+++ config-server-vault/src/test/java/de/dominikschadow/configserver/vault/ConfigServerVaultApplicationTest.java
@@ -29,7 +29,7 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApplicationTest {
+public class ConfigServerVaultApplicationTest {
     @Test
     public void contextLoads() {
     }
