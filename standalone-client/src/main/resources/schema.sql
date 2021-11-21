69fc4c1344bfa407d3a33d15548afbbb5cdef4ea
Dominik Schadow <dominikschadow@googlemail.com>
2021-11-21 13:06:24 +0100

    db init via data.sql and schema.sql

--- standalone-client/src/main/resources/schema.sql
+++ standalone-client/src/main/resources/schema.sql
@@ -1,15 +1,15 @@
-CREATE TABLE user (
+CREATE TABLE client_users (
     id SERIAL PRIMARY KEY,
     firstname VARCHAR NOT NULL,
     lastname VARCHAR NOT NULL
 );
 
-CREATE TABLE credential (
+CREATE TABLE client_credentials (
     id SERIAL PRIMARY KEY,
     user_id INT,
     username VARCHAR NOT NULL,
     password VARCHAR NOT NULL,
     CONSTRAINT fk_user
         FOREIGN KEY(user_id)
-            REFERENCES user(id)
+            REFERENCES client_users(id)
 );
